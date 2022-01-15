63871f14eedde75dd0c27ed172df73fa97ef5a1c
Lukasz Plawiak <lukaszplawiakzero1@gmail.com>
2022-01-15 08:44:25 +0100

    sixteen commit: refactor of Warmup class

--- src/main/java/com/lukaszplawiak/projectapp/ProjectAppApplication.java
+++ src/main/java/com/lukaszplawiak/projectapp/ProjectAppApplication.java
@@ -1,9 +1,5 @@
 package com.lukaszplawiak.projectapp;
 
-import com.lukaszplawiak.projectapp.dto.UserRequestDto;
-import com.lukaszplawiak.projectapp.model.Role;
-import com.lukaszplawiak.projectapp.service.UserService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -15,31 +11,6 @@ public class ProjectAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectAppApplication.class, args);
-    }
-
-    @Bean
-    CommandLineRunner run(UserService userService) {
-        return args -> {
-          userService.saveRole(new Role(null, "ROLE_USER"));
-          userService.saveRole(new Role(null, "ROLE_MANAGER"));
-          userService.saveRole(new Role(null, "ROLE_ADMIN"));
-          userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
-
-          userService.saveUser(UserRequestDto.UserRequestDtoBuilder.anUserRequestDto().withFirstName("Luk").withLastName("PLa").withEmail("lukpla@gmail.com").withPassword("1234").build());
-          userService.saveUser(UserRequestDto.UserRequestDtoBuilder.anUserRequestDto().withFirstName("Ada").withLastName("Mala").withEmail("adamala@gmail.com").withPassword("1234").build());
-          userService.saveUser(UserRequestDto.UserRequestDtoBuilder.anUserRequestDto().withFirstName("Ula").withLastName("Lula").withEmail("ulalula@gmail.com").withPassword("1234").build());
-          userService.saveUser(UserRequestDto.UserRequestDtoBuilder.anUserRequestDto().withFirstName("Ala").withLastName("Hala").withEmail("alahala@gmail.com").withPassword("1234").build());
-
-
-          userService.addRoleToUser("lukpla@gmail.com", "ROLE_USER");
-          userService.addRoleToUser("lukpla@gmail.com", "ROLE_MANAGER");
-          userService.addRoleToUser("lukpla@gmail.com", "ROLE_ADMIN");
-          userService.addRoleToUser("adamala@gmail.com", "ROLE_MANAGER");
-          userService.addRoleToUser("ulalula@gmail.com", "ROLE_ADMIN");
-          userService.addRoleToUser("alahala@gmail.com", "ROLE_SUPER_ADMIN");
-          userService.addRoleToUser("alahala@gmail.com", "ROLE_ADMIN");
-          userService.addRoleToUser("alahala@gmail.com", "ROLE_USER");
-        };
     }
 
     @Bean
--- src/main/java/com/lukaszplawiak/projectapp/config/Warmup.java
+++ src/main/java/com/lukaszplawiak/projectapp/config/Warmup.java
@@ -1,8 +1,39 @@
 package com.lukaszplawiak.projectapp.config;
 
+import com.lukaszplawiak.projectapp.dto.UserRequestDto;
+import com.lukaszplawiak.projectapp.model.Role;
+import com.lukaszplawiak.projectapp.service.UserService;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Warmup {
-    //dane do zaczytania do db
+public class Warmup implements ApplicationListener<ContextRefreshedEvent> {
+    private final UserService userService;
+
+    public Warmup(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public void onApplicationEvent(final ContextRefreshedEvent event) {
+        userService.saveRole(new Role(null, "ROLE_USER"));
+        userService.saveRole(new Role(null, "ROLE_MANAGER"));
+        userService.saveRole(new Role(null, "ROLE_ADMIN"));
+        userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+
+        userService.saveUser(UserRequestDto.UserRequestDtoBuilder.anUserRequestDto().withFirstName("Luk").withLastName("PLa").withEmail("lukpla@gmail.com").withPassword("1234").build());
+        userService.saveUser(UserRequestDto.UserRequestDtoBuilder.anUserRequestDto().withFirstName("Ada").withLastName("Mala").withEmail("adamala@gmail.com").withPassword("1234").build());
+        userService.saveUser(UserRequestDto.UserRequestDtoBuilder.anUserRequestDto().withFirstName("Ula").withLastName("Lula").withEmail("ulalula@gmail.com").withPassword("1234").build());
+        userService.saveUser(UserRequestDto.UserRequestDtoBuilder.anUserRequestDto().withFirstName("Ala").withLastName("Hala").withEmail("alahala@gmail.com").withPassword("1234").build());
+
+        userService.addRoleToUser("lukpla@gmail.com", "ROLE_USER");
+        userService.addRoleToUser("lukpla@gmail.com", "ROLE_MANAGER");
+        userService.addRoleToUser("lukpla@gmail.com", "ROLE_ADMIN");
+        userService.addRoleToUser("adamala@gmail.com", "ROLE_MANAGER");
+        userService.addRoleToUser("ulalula@gmail.com", "ROLE_ADMIN");
+        userService.addRoleToUser("alahala@gmail.com", "ROLE_SUPER_ADMIN");
+        userService.addRoleToUser("alahala@gmail.com", "ROLE_ADMIN");
+        userService.addRoleToUser("alahala@gmail.com", "ROLE_USER");
+    }
 }
